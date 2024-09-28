bb7b103522d048a85991e5c7f5b54c64b447215d
CozyBrian <dragonbrian33@gmail.com>
2024-09-28 02:17:02 +0000

    add: Events page and Event Details page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [
-      './**/templates/*.html',
+  content: [
+      './**/templates/**/*.html',
   ],
   theme: {
     extend: {
