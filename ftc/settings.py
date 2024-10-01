31be0a5a267f6db317400dd602e1e414c49f4f3b
Saviour Gidi <profsaviour@gmail.com>
2024-10-01 09:07:23 +0200

    brought back gcp creds

--- ftc/settings.py
+++ ftc/settings.py
@@ -154,9 +154,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 
-# GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
-#     os.path.join(BASE_DIR, "gcpCredentials.json")
-# )
+GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
+    os.path.join(BASE_DIR, "gcpCredentials.json")
+)
 
 GS_BUCKET_NAME = env("GS_BUCKET_NAME")
 STORAGES = {
