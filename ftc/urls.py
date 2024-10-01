2e4f8da04a47c2ff5f0c6077fcf601ef2f684944
Kwame Akuamoah-Boateng <kaboateng14@gmail.com>
2024-10-01 10:51:46 +0000

    Remove events from urls

--- ftc/urls.py
+++ ftc/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("events/", include("update.urls")),
+    # path("events/", include("update.urls")),
     path("api/", include("api.urls")),
 ]
 
